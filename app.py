fdbc57621cb1b7eff9d2c4a14b9aeb8685f1e869
Aquila <evgen-vladim@yandex.ru>
2020-08-23 15:17:32 +0500

    Updated app

--- app.py
+++ app.py
@@ -15,6 +15,35 @@ import os
 import json
 from ast import literal_eval
 import traceback
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+import datetime
+import requests
+import collections as co
+import string
+import re
+
+import nltk
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+from sumy.utils import get_stop_words as gsw1
+from stop_words import get_stop_words as gsw2
+
+
+import pandas as pd
+import numpy as np
+
+from io import StringIO
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+%matplotlib inline
+
+import pickle
+import re
+from nltk.stem.porter import *
+stemming = PorterStemmer()
 
 application = Flask(__name__)
 
@@ -35,34 +64,78 @@ def hello():
     
     return response
 
-#предобработка сообщения
-def processing_message(message):
-    true_words = []
-    words = re.split(' ', message)
-    for word in words:
-        m = re.search('(\w+)', word)
-        if m is not None:
-            good_word = m.group(0)
-            true_words.append(good_word)
-    stemming = PorterStemmer()
-    stemmed_message_list = [stemming.stem(word) for word in true_words]
-    stemmed_message = ' '.join(stemmed_message_list)
-    return stemmed_message
+
+def cleaning_message(text):
+    text = re.sub('\[.*\]', '', text)
+    text = re.sub("\!", '', text)
+    text = re.sub("\'", '', text)
+    text = re.sub("[^A-Za-z0-9^,!.\/'+-=]", ' ', text)
+    text = re.sub("\s+", ' ', text)
+    return text
+
+
+def get_original_form(text):
+    try:
+        words = re.split(' ', text)
+        true_words = []
+        for word in words:
+            m = re.search('(\w+)', word)
+            if m is not None:
+                good_word = m.group(0)
+                true_words.append(good_word)
+        tagged = nltk.pos_tag(true_words)
+        tags = []
+        for tag in tagged:
+            tags.append(tag[1])
+    except:
+        pass
+
+    stopWords = nltk.corpus.stopwords.words()
+    LANGUAGE = 'english'
+
+    sw0 = ["yeah", "zola", "don"]
+    sw1 = gsw1(LANGUAGE)
+    sw2 = gsw2('en')
+
+    sw0.extend(list(sw1))
+    sw0.extend(list(sw2))
+
+    new_s = ''
+    new_lw = []
+    new_lt = []
+    new_lwt = []
+    for w, t, lw in zip(true_words, tags, tagged):
+        if t in ['NN', 'VB', 'DT', 'NNS', 'VBP', 'VB']:
+            new_s += w + ' '
+            new_lw.append(w)
+            new_lt.append(t)
+            new_lwt.append(lw)
+        elif w in sw0 or re.match('\d+', w) is not None:
+            continue
+    new_list = re.split(r'\W+', new_s)
+    stemmed_list = [stemming.stem(word) for word in new_list]
+    original_form = ' '.join(stemmed_list)
+    return original_form
 
 # предикт категории
 @application.route("/categoryPrediction" , methods=['GET', 'POST'])  
 def registration():
     resp = {'message':'ok'
-           ,'category': -1
+           ,'prediction': -1
            }
 
     try:
         getData = request.get_data()
-        json_params = json.loads(getData) 
+        json_params = json.loads(getData)
+
+        message = json_params['user_message']
+        message = message.lower()
+        message = cleaning_message(message)
+        message = get_original_form(message)
         
         #напишите прогноз и верните его в ответе в параметре 'prediction'
-        category = model.predict_proba(vec.transform([json_params['user_message']]).toarray()).tolist()
-        resp['category'] = category
+        prediction = model.predict_proba(vec.transform([message]).toarray()).tolist()
+        resp['prediction'] = prediction
 
         
     except Exception as e: 
