dc7883feaa75ecc4b59cb948a02958445295c52e
Aquila <evgen-vladim@yandex.ru>
2020-08-23 15:26:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -15,35 +15,7 @@ import os
 import json
 from ast import literal_eval
 import traceback
-from sklearn.feature_extraction.text import TfidfVectorizer
 
-import datetime
-import requests
-import collections as co
-import string
-import re
-
-import nltk
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
-from sumy.utils import get_stop_words as gsw1
-from stop_words import get_stop_words as gsw2
-
-
-import pandas as pd
-import numpy as np
-
-from io import StringIO
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-%matplotlib inline
-
-import pickle
-import re
-from nltk.stem.porter import *
-stemming = PorterStemmer()
 
 application = Flask(__name__)
 
@@ -75,47 +47,8 @@ def cleaning_message(text):
 
 
 def get_original_form(text):
-    try:
-        words = re.split(' ', text)
-        true_words = []
-        for word in words:
-            m = re.search('(\w+)', word)
-            if m is not None:
-                good_word = m.group(0)
-                true_words.append(good_word)
-        tagged = nltk.pos_tag(true_words)
-        tags = []
-        for tag in tagged:
-            tags.append(tag[1])
-    except:
-        pass
 
-    stopWords = nltk.corpus.stopwords.words()
-    LANGUAGE = 'english'
-
-    sw0 = ["yeah", "zola", "don"]
-    sw1 = gsw1(LANGUAGE)
-    sw2 = gsw2('en')
-
-    sw0.extend(list(sw1))
-    sw0.extend(list(sw2))
-
-    new_s = ''
-    new_lw = []
-    new_lt = []
-    new_lwt = []
-    for w, t, lw in zip(true_words, tags, tagged):
-        if t in ['NN', 'VB', 'DT', 'NNS', 'VBP', 'VB']:
-            new_s += w + ' '
-            new_lw.append(w)
-            new_lt.append(t)
-            new_lwt.append(lw)
-        elif w in sw0 or re.match('\d+', w) is not None:
-            continue
-    new_list = re.split(r'\W+', new_s)
-    stemmed_list = [stemming.stem(word) for word in new_list]
-    original_form = ' '.join(stemmed_list)
-    return original_form
+    return text
 
 # предикт категории
 @application.route("/categoryPrediction" , methods=['GET', 'POST'])  
